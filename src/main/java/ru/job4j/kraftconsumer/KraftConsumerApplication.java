9e26d1727c339a71f8f16edf2b80de4f0af7cd88
AlexeyEsipov <a_esipov@list.ru>
2025-09-05 09:22:37 +0300

    init commit

--- src/main/java/ru/job4j/kraftconsumer/KraftConsumerApplication.java
+++ src/main/java/ru/job4j/kraftconsumer/KraftConsumerApplication.java
@@ -6,9 +6,9 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class EmailnotificationApplication {
+public class KraftConsumerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EmailnotificationApplication.class, args);
+		SpringApplication.run(KraftConsumerApplication.class, args);
 	}
 }
